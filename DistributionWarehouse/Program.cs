18e9676502004dc5310417d7819ac60881fb17f2
Madalina <petrus_andreeamadalina@yahoo.com>
2022-08-02 16:06:26 +0300

    Add project files.

---
+++ DistributionWarehouse/Program.cs
@@ -0,0 +1,17 @@
+﻿using DistributionWarehouse;
+public class Hello
+{
+
+    public static void Main(string[] args)
+    {
+        Product p = new Product()
+        {
+            Name = "ceva",
+            Unit = "kg",
+            Price = 12.5F
+        };
+        Console.WriteLine(p.ToString());
+     
+
+    }
+}
---
+++ DistributionWarehouse/Vegetable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributionWarehouse
+{
+    internal class Vegetable:Product
+    {
+        public string NutritionalQuality { get; set; }
+        public string Producer { get; set; }
+    }
+}
